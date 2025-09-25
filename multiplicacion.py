f89b1454563402204467e70a6d1327c459f2a493
YISUS-CAMPOS <a.campoos10@gmail.com>
2025-09-25 08:29:37 -0600

    Tiny_toons_Calculator
    The project is about the design and development of a digital calculator, the tool offer a simple and intuitive interface for users.

---
+++ multiplicacion.py
@@ -0,0 +1,4 @@
+numero1=float(input("Ingresa una cantidad al primer numero: "))
+numero2=float(input("Ingresa una cantidad al segundo numero: "))
+multiplicacion = numero1 * numero2
+print("El resultado de ", numero1, "+", numero2, "es: ", multiplicacion)
